b1de80ee248c80f1c79936ff5ac8e48bfbc6aaa6
kam1kazy <t2nda@yandex.ru>
2022-11-24 17:02:06 +0300

    поправил работу input с названием компании, убрал стейт

--- src/components/form/CreateCompany.js
+++ src/components/form/CreateCompany.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import ProductList from "./ProductList";
--- src/components/form/inputs/InputField.js
+++ src/components/form/inputs/InputField.js
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { ErrorMessage, Field } from "formik";
 import { TextField } from "@mui/material";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { changeName } from "../../../redux/slice/companyNameSlice";
 
 export default function InputField({ name, label, type }) {
-  const [nameCompany, setNameCompany] = useState("");
   const dispatch = useDispatch();
+  const companyName = useSelector((state) => state.companyNameReducer.name);
 
-  useEffect(() => {
-    dispatch(changeName(nameCompany));
-  }, [nameCompany]);
+  const handleCheckInput = (e) => {
+    dispatch(changeName(e.target.value));
+  };
 
   return (
     <Field
       required
       autoComplete="off"
+      value={companyName}
       name={name}
       label={label}
       type={type}
       as={TextField}
-      onChange={(e) => setNameCompany(e.target.value)}
+      onChange={handleCheckInput}
       helperText={<ErrorMessage name={name} />}
     />
   );
